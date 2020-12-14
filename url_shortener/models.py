21b9b20f98db0e44f6468cd9a26c419c8168a238
KaiJie <57020975+kjc1998@users.noreply.github.com>
2020-12-14 00:30:09 +0000

    Initial commit

---
+++ url_shortener/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from .extensions import db
+
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(512))
+    short_url = db.Column(db.String(3), unique=True)
+    visits = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.short_url = 'something'
+
+    def generate_short_link(self):
+        return
