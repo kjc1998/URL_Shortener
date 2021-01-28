2da8b3f51754c396fbcd559fdf7814ff81608dc4
Kai Jie Chow <kj1chow@gmail.com>
2021-01-28 02:39:43 +0000

    postgres

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -1,12 +1,12 @@
-﻿from flask import Flask
+﻿import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import url_shortener.routes
 from .extensions import db, login_manager, bcrypt, api
-from .settings import SQLALCHEMY_DATABASE_URI
 
 
 appF = Flask(__name__)
-appF.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+appF.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 appF.config['SECRET_KEY'] = '95e1f4ae670667e3338bd65cfe36c773d9b958bb'
 appF.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 appF.register_blueprint(url_shortener.routes.app)
