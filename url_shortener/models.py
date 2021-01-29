7bb3eb484784acab0d01488657b72cd18ddf595b
Kai Jie Chow <kj1chow@gmail.com>
2021-01-29 01:49:33 +0000

    notfixed

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -7,7 +7,7 @@ from .extensions import db, login_manager
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
