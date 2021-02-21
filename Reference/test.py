2ec21a7b528e2c86cbd02054aba54631f3e992da
Kai Jie Chow <kj1chow@gmail.com>
2021-02-21 12:11:18 +0000

    API changes

--- Reference/test.py
+++ Reference/test.py
@@ -5,12 +5,11 @@ import urllib
 #userid = post request (username & password)#
 
 BASE = "https://spshurl.herokuapp.com/"
+BASE_LOCAL = "http://127.0.0.1:5000/"
 long_link = "https://www.youtube.com"
 
 userID = (requests.post(BASE + "login/" +
                         "cer0cifer" + "/kaijie")).json()['userid']
-print(userID)
-'''short = requests.post(
-    BASE+"details/"+userID+'/'+long_link)
-shortened = short.json()['short_url']
-print(shortened)'''
+short = requests.post(
+    BASE + "details/"+userID+'/'+long_link)
+shortened = short.json()
