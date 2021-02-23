0a924ad50f705e1e3613ccdd52049dc9a69de337
Kai Jie Chow <kj1chow@gmail.com>
2021-02-23 17:20:57 +0000

    script at layout

--- url_shortener/static/index.js
+++ url_shortener/static/index.js
@@ -1,8 +1,3 @@
-window.addEventListener('resize', ()=>{
-    let vh = window.innerHeight*0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 const navSlide = () => {
     const burger = document.querySelector('.burgerView');
     const nav = document.querySelector('.rightSide');
