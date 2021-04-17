ddb67cad88f9c16e8d02533df61f91d5b62b33a2
Kai Jie Chow <kj1chow@gmail.com>
2021-04-17 11:43:24 +0100

    test.py udpates

--- Reference/test.py
+++ Reference/test.py
@@ -8,8 +8,8 @@ userID = (requests.post(BASE + "login/" + USERNAME + "/" + PASSWORD).json())['us
 print(userID)
 
 # url to be shortened or extracted
-URL = urllib.parse.urlparse("https://docs.katalon.com/katalon-studio/videos/create_basic_automation_test_case_katalon_studio.html")
-postLink = BASE + "details/" + f"{userID}/" + URL.geturl()
+URL = "https://docs.katalon.com/katalon-studio/videos/create_basic_automation_test_case_katalon_studio.html"
+postLink = BASE + "details/" + f"{userID}/" + URL
 
 details = requests.post(postLink).json()
 print(details)
