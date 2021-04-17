286ea2374555ab3ff19f3eb588ad6474c66c6f22
Kai Jie Chow <kj1chow@gmail.com>
2021-04-17 11:42:44 +0100

    test.py updates

--- Reference/test.py
+++ Reference/test.py
@@ -1,6 +1,15 @@
 import json
 import requests
+import urllib
 
-BASE = "http://127.0.0.1:5000/"
-userID = requests.post(BASE + "login/" + "jeremyleejr/" + "Jlee1998$")
-print(userID.json())
+USERNAME, PASSWORD = "cer0cifer", "Ckj1ckj1"
+BASE = "https://spshurl.herokuapp.com/"
+userID = (requests.post(BASE + "login/" + USERNAME + "/" + PASSWORD).json())['userid']
+print(userID)
+
+# url to be shortened or extracted
+URL = urllib.parse.urlparse("https://docs.katalon.com/katalon-studio/videos/create_basic_automation_test_case_katalon_studio.html")
+postLink = BASE + "details/" + f"{userID}/" + URL.geturl()
+
+details = requests.post(postLink).json()
+print(details)
